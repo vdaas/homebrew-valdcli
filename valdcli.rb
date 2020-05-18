5a06ae3536725c50949b17fa30c259ca1706e983
VDaaS org <ci@vdaas.org>
2020-05-18 02:07:40 +0000

    :robot: Update valdcli v0.0.36

--- valdcli.rb
+++ valdcli.rb
@@ -1,9 +1,9 @@
 class Valdcli < Formula
   desc "A CLI client tool for Vald."
   homepage "https://clojars.org/vald-client-clj"
-  version "v0.0.35"
+  version "v0.0.36"
   url "https://github.com/vdaas/vald-client-clj/releases/download/#{version}/valdcli-macos.zip"
-  sha256 "d30dd37dfbd499c775fe0bbae2ff3e7bf4e04fd5bf3ec407ccc28b1b7aceb215"
+  sha256 "101e5e05f7716e9a14e1ff6686914459951df195fccb177665289984ed08feeb"
 
   def install
     bin.install "valdcli" => "valdcli"
